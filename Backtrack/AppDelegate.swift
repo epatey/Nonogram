8e48eb524c019f9b1fc05bbcd6f07a77dec6ee80
Eric Patey <eric@patey.com>
2015-12-14 13:20:52 -0500

    Initial Commit

---
+++ Backtrack/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Backtrack
+//
+//  Created by Eric Patey on 12/14/15.
+//  Copyright © 2015 bouldertop. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
